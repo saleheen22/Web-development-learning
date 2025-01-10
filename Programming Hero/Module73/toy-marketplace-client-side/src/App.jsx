41ade55a4612bb43c8af99e471aa544221544b13
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-01-09 19:37:09 -0600

    Initial commit: Added the first set of code

---
+++ Programming Hero/Module73/toy-marketplace-client-side/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Font, { Text } from 'react-font'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      
+        {/* <Font family='Roboto'>
+        <h2  className='font-abc'>Hello World</h2>
+        </Font> */}
+        <h2 className='font-abc'>Hello World</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
